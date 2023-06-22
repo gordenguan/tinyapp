412df93bfb9f2846d59158bc6624182f524b46b0
Gordenguan <gordenguan122@gmail.com>
2023-06-22 13:54:48 -0600

    Add route and send json string

--- express_server.js
+++ express_server.js
@@ -5,12 +5,16 @@ const PORT = 8080;    //default port 8080
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-}
+};
 
 app.get('/', (req, res) => {
-  res.send('Hello!')
+  res.send('Hello!');
+});
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+  console.log(`Example app listening on port ${PORT}`);
 });
